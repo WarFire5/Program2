22fc6a443425a6bf2c8e891652d230723f8f0fc9
Татьяна <@WIN-E362SIULNK8>
2023-02-02 02:11:30 +0300

    Добавьте файлы проекта.

---
+++ Program2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string clientFirstName;
+            string clientLastName;
+            string clientLastSecondName;
+
+            Console.WriteLine("Как Вас зовут?");
+            clientFirstName = Console.ReadLine();
+
+            Console.WriteLine("Какая у Вас фамилия?");
+            clientLastName = Console.ReadLine();
+
+            Console.WriteLine("Как Ваше отчество?");
+            clientLastSecondName = Console.ReadLine();
+
+            string clientAllName = clientFirstName + " " + clientLastName + " " + clientLastSecondName;
+            Console.WriteLine(clientAllName);
+
+            Console.ReadLine();
+        }
+    }
+}
